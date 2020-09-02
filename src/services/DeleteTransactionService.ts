ea791f49b237022d7b96cbc8d9f50d82943f2e6e
Ana Paula Moraes <ana@anawd.com>
2020-09-02 01:33:58 -0300

    Desafio 6 Finalizado

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transatrionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transatrionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transatrionsRepository.remove(transaction);
   }
 }
 
